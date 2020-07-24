5faa5560235ed4b886d781307feeac65de104f8d
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2020-07-24 19:51:01 -0300

    Comparing Error Rate with the K Value

--- knn-regression.py
+++ knn-regression.py
@@ -26,3 +26,21 @@ X_test = scaler.transform(X_test)
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+# Evaluating the Algorithm
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# Comparing Error Rate with the K Value
+error = []
+
+# Calculating error for K values between 1 and 40
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error.append(np.mean(pred_i != y_test))
+    
