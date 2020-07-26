c0993bc47d59cc72736bf55a178845ef9fe724ba
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2020-07-25 21:14:00 -0300

    plot

--- knn-regression.py
+++ knn-regression.py
@@ -44,3 +44,10 @@ for i in range(1, 40):
     pred_i = knn.predict(X_test)
     error.append(np.mean(pred_i != y_test))
     
+plt.figure(figsize=(12, 6))
+plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',
+         markerfacecolor='blue', markersize=10)
+plt.title('Error Rate K Value')
+plt.xlabel('K Value')
+plt.ylabel('Mean Error')
+
